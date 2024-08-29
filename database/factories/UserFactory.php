bfabd0fabdab2afb178847d9024a3b3df3590600
AditNomous <aditmobilelegends@gmail.com>
2024-08-29 16:55:08 +0700

    10. model factoris

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,9 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            // $password = $password ? $password : $Newpassword   //ternary operator
+            // $password = $password ?: $Newpassword //elvisoperator
+            // $password ??= $ Newpassword   //null coalescing operator
             'remember_token' => Str::random(10),
         ];
     }
