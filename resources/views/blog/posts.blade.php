da4b646894f00124c018cf751ddfef4c916b8919
AditNomous <aditmobilelegends@gmail.com>
2024-09-18 09:16:20 +0700

    ui posts

--- resources/views/blog/posts.blade.php
+++ resources/views/blog/posts.blade.php
@@ -31,7 +31,7 @@
 
         </form>
         <a href="/create"
-            class="text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  ">
+            class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  ">
             Create Post </a>
     
      
@@ -52,11 +52,11 @@
                                     </svg>
                                     {{ $post->category->name }}
                                 </a>
-                                <a class="bg-yellow-100 px-5 py-2 rounded-lg p-1" href="/edit/{{$post->id}}">Edit</a>
-                                <a href="/delete/{{$post->id}}"
-                                    class="text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  ">
-                                    Delete </a>
                                 <span class="text-sm">{{ $post->created_at->diffForHumans() }}</span>
+                                <a class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-2.5 text-center  " href="/edit/{{$post->id}}">Edit</a>
+                                <a href="/delete/{{$post->id}}"
+                                    class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-2.5 text-center  ">
+                                    Delete </a>
                             </div>
                             <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
                                     href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
