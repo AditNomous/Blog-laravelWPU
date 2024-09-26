8b7e31e5a9c8e5ccf70cfbbad9ea8dcfe78ab8e2
AditNomous <aditmobilelegends@gmail.com>
2024-09-26 14:18:59 +0700

    favicon

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,10 +3,11 @@
       <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
               <div class="flex-shrink-0">
-                  <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
-              </div>
-              <div class="hidden md:block">
-                  <div class="ml-10 flex items-baseline space-x-4">
+                  <img class="w-10" src="img\icon.png">
+                </div>
+                <div class=" ml-2 font-bold">WhatPost</div>
+                <div class="hidden md:block">
+                    <div class="ml-10 flex items-baseline space-x-4">
                       <x-nav-link href="/" :active="request()->is('home')">Home</x-nav-link>
                       <x-nav-link href="/posts" :active="request()->is('posts')">Blog</x-nav-link>
                       @auth
